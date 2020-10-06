163ddf6d87411f6d7432e98e4a75244e2352b73d
Eugene Zuev <zhekazuev@gmail.com>
2020-10-06 14:37:32 +0300

    Add information

--- push_backup_script.py
+++ push_backup_script.py
@@ -26,18 +26,20 @@ def main():
     ap_addresses = set(ap_addresses)
 
     for ap_address in ap_addresses:
+        print(f"Work with {ap_address}")
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname=ap_address, username=user, password=password)
         stdin, stdout, stderr = ssh.exec_command(f"system script add name=autobackup source=\"{script}\"")
         script_create_result = stdout.read().decode("utf8")
-        print(f"Created backup script result: {script_create_result}")
+        print(f"\tCreated backup script result: {script_create_result}")
         stdin, stdout, stderr = ssh.exec_command(f"system scheduler add name=config_backup "
                                                  f"on-event=\"/system script run autobackup\" "
                                                  f"policy=ftp,read,write,test,password,romon,sniff,policy,sensitive "
                                                  f"start-time=\"05:00:00\" interval=\"1d 00:00:00\"")
         schedule_create_result = stdout.read().decode("utf8")
-        print(f"Created schedule result: {schedule_create_result}")
+        print(f"\tCreated schedule result: {schedule_create_result}")
+        print(f"End with {ap_address}")
         ssh.close()
 
 
