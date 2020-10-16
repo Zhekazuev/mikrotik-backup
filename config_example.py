33f8b7112763b7177df2f5e11cf4bd1c56049694
Eugene Zuev <zhekazuev@gmail.com>
2020-10-16 11:56:25 +0300

    Add functions for getting AP IP addresses from WLCs and HUBs Change script for backup on mikrotik

--- config_example.py
+++ config_example.py
@@ -2,9 +2,36 @@ ap_user = "user"
 ap_password = "pass"
 hub_user = "user"
 hub_password = "pass"
-ips_hubs = ["1.1.1.1", "1.1.1.2", "1.1.1.3"]
+ips_hubs = ["1.1.1.1", "1.1.1.2"]
+ips_wlcs = ["2.2.2.1", "2.2.2.2", "2.2.2.3", "2.2.2.4"]
 
 
-script = """#Backup configuration Mikrotik\r\n
-#Get date in format dd-mm-yyyy\n\r
-:local tmpdate [/system clock get date];\r\n"""
+script = ["#Backup configuration Mikrotik\\r\\n",
+          "#Get date in format dd-mm-yyyy\\r\\n",
+          ":local tmpdate [/system clock get date];\\r\\n",
+          ":local months (\\\"jan\\\",\\\"feb\\\",\\\"mar\\\",\\\"apr\\\",\\\"may\\\",\\\"jun\\\",\\\"jul\\\",\\\"aug\\\",\\\"sep\\\",\\\"oct\\\",\\\"nov\\\",\\\"dec\\\");\\r\\n",
+          ":local month [ :pick \$tmpdate 0 3 ];\\r\\n",
+          ":local mm ([ :find \$months \$month -1 ] + 1);\\r\\n",
+          ":if (\$mm < 10) do={ :set mm (\\\"0\\\" . \$mm); }\\r\\n",
+          ":local date ([:pick \$tmpdate 4 6] .\\\"-\\\" . \$mm .\\\"-\\\" . [:pick \$tmpdate 7 11])\\r\\n\\r\\n",
+          "#Create variables and parameters for FTP\\r\\n\\r\\n",
+          ":local myname [/system identity get name]\\r\\n",
+          ":local fname (\$myname.\\\"_\\\".\$date);\\r\\n:local bname (\$myname.\\\"_\\\".\$date.\\\".backup\\\");\\r\\n",
+          ":local ename (\$myname.\\\"_\\\".\$date.\\\".rsc\\\");\\r\\n",
+          ":local ftpuser \\\"user\\\";\\r\\n",
+          ":local ftppass \\\"pass\\\";\\r\\n",
+          ":local ftpaddr \\\"address\\\";\\r\\n",
+          ":local ftppath \\\"ftppath\\\"\\r\\n\\r\\n",
+          "#Get system parameters\\r\\n\\r\\n",
+          "/system backup save name=\$fname password=\\\"\\\";\\r\\n",
+          ":delay 10;\\r\\n",
+          "/export file=\$fname\\r\\n",
+          ":delay 10;\\r\\n\\r\\n",
+          "#Push configuration to FTP\\r\\n\\r\\n",
+          "/tool fetch address=\$ftpaddr src-path=\$bname user=\$ftpuser password=\$ftppass port=21 upload=yes mode=ftp dst-path=\\\"\$ftppath/\$bname\\\"\\r\\n",
+          ":delay 15;\\r\\n",
+          "/tool fetch address=\$ftpaddr src-path=\$ename user=\$ftpuser password=\$ftppass port=21 upload=yes mode=ftp dst-path=\\\"\$ftppath/\$ename\\\"\\r\\n",
+          ":delay 15;\\r\\n\\r\\n",
+          "#Remove backup on mikrotik\\r\\n",
+          "/file remove \$bname;\\r\\n",
+          "/file remove \$ename;\\r\\n"]
--- push_backup_script.py
+++ push_backup_script.py
@@ -9,11 +9,22 @@ ap_user = config.ap_user
 ap_password = config.ap_password
 
 
-def get_aps_ips(ip_address):
+def get_aps_ips_wlc(ip_address):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname=ip_address, username=hub_user, password=hub_password)
-    stdin, stdout, stderr = ssh.exec_command("interface eoip print detail where running")
+    stdin, stdout, stderr = ssh.exec_command("caps-man remote-cap print detail terse")
+    caps_interfaces = stdout.read().decode("utf8")
+    ip_address = re.findall(r"\s+address=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/\d+\s+", caps_interfaces)
+    ssh.close()
+    return ip_address
+
+
+def get_aps_ips_hubs(ip_address):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=ip_address, username=hub_user, password=hub_password)
+    stdin, stdout, stderr = ssh.exec_command("interface eoip print detail without-paging where running")
     eoip_interfaces = stdout.read().decode("utf8")
     ip_address = re.findall(r"\s+remote-address=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+", eoip_interfaces)
     ssh.close()
@@ -21,28 +32,47 @@ def get_aps_ips(ip_address):
 
 
 def main():
-    script = config.script
-    ips_hubs = config.ips_hubs
+    lines = config.script
+    script = ""
+    for line in lines:
+        script += line
+
     ap_addresses = []
-    for ip_hub in ips_hubs:
-        ap_addresses.extend(get_aps_ips(ip_hub))
+
+    ips_wlcs = config.ips_wlcs
+    for ip_wlc in ips_wlcs:
+        print(f"Work with {ip_wlc}")
+        ap_addresses.extend(get_aps_ips_wlc(ip_wlc))
+        print(f"The addresses from {ip_wlc} were successfully taken")
+
+    ips_hubs = config.ips_hubs
+    for ip_wlc in ips_wlcs:
+        print(f"Work with {ip_wlc}")
+        ap_addresses.extend(get_aps_ips_hubs(ips_hubs))
+        print(f"The addresses from {ip_wlc} were successfully taken")
+
     ap_addresses = set(ap_addresses)
+    print(f"Total AP: {len(ap_addresses)}")
 
     for ap_address in ap_addresses:
         print(f"Work with {ap_address}")
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=ap_address, username=ap_user, password=ap_password)
-        stdin, stdout, stderr = ssh.exec_command(f"system script add name=autobackup source=\"{script}\"")
-        script_create_result = stdout.read().decode("utf8")
-        print(f"\tCreated backup script result: {script_create_result}")
-        stdin, stdout, stderr = ssh.exec_command(f"system scheduler add name=config_backup "
-                                                 f"on-event=\"/system script run autobackup\" "
-                                                 f"policy=ftp,read,write,test,password,romon,sniff,policy,sensitive "
-                                                 f"start-time=\"05:00:00\" interval=\"1d 00:00:00\"")
-        schedule_create_result = stdout.read().decode("utf8")
-        print(f"\tCreated schedule result: {schedule_create_result}")
-        print(f"End with {ap_address}")
+        try:
+            ssh.connect(hostname=ap_address, username=ap_user, password=ap_password)
+            print(f"Problem with {ap_address}")
+            stdin, stdout, stderr = ssh.exec_command(f"system script add name=autobackup source=\"{script}\"")
+            script_create_result = stdout.read().decode("utf8")
+            print(f"\tCreated backup script result: {script_create_result}")
+            stdin, stdout, stderr = ssh.exec_command(f"system scheduler add name=autobackup "
+                                                     f"on-event=\"/system script run autobackup\" "
+                                                     f"policy=ftp,read,write,test,password,romon,sniff,policy,sensitive "
+                                                     f"start-time=\"05:00:00\" interval=\"1d 00:00:00\"")
+            schedule_create_result = stdout.read().decode("utf8")
+            print(f"\tCreated schedule result: {schedule_create_result}")
+            print(f"End with {ap_address}")
+        except paramiko.SSHException:
+            print(f"Problem with {ap_address}")
         ssh.close()
 
 
